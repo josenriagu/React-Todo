2d8d92cbf39889ec716dae1902c57dae59c9a5b4
Josemaria <josemarianriagu25@gmail.com>
2019-05-29 21:36:00 +0800

    Import components into App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,93 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+const InitialtoDoCrate = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoCrate: InitialtoDoCrate,
+      newTodo: ""
+    };
+  }
+
+  changeTodo = event => {
+    this.setState({
+      newTodo: event.target.value
+    });
+  };
+
+  addTodo = () => {
+    const newTodo = {
+      task: this.state.newTodo,
+      id: Date.now(),
+      completed: false
+    };
+
+    const newtodoCrate = this.state.toDoCrate.concat(newTodo);
+
+    this.setState({
+      toDoCrate: newtodoCrate,
+      newTodo: ""
+    });
+  };
+
+  markCompleted = event => {
+    let toDoCrateNew = this.state.toDoCrate.map(todo => {
+      if (todo.id === parseInt(event.target.id)) {
+        todo.completed = true;
+      }
+      return todo;
+    });
+
+    this.setState({
+      toDoCrate: toDoCrateNew
+    });
+  };
+
+  clearTodo = event => {
+    let uncompletedTodo = this.state.toDoCrate.filter(todo => !todo.completed);
+
+    this.setState({
+      toDoCrate: uncompletedTodo
+    });
+  };
+
+  addOnEnter = event => {
+    let keyPressed = event.keyCode ? event.KeyCode : event.which;
+    if (keyPressed === 13) {
+      this.addTodo();
+    }
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList
+          todo={this.state.toDoCrate}
+          markCompleted={this.markCompleted}
+        />
+        <TodoForm
+          value={this.state.newTodo}
+          changeHandler={this.changeTodo}
+          addTodo={this.addTodo}
+          clearTodo={this.clearTodo}
+          addOnEnter={this.addOnEnter}
+        />
       </div>
     );
   }
