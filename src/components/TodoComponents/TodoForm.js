3d1070fc3f21d6863d8e2155b65b868e555f2d56
Josemaria <josemarianriagu25@gmail.com>
2019-05-30 21:09:22 +0800

    Refactor class components to functional components

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,20 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Todo.css";
 
-export default class TodoForm extends Component {
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="...todo"
-          value={this.props.value}
-          onChange={this.props.changeHandler}
-          onKeyPress={this.props.addOnEnter}
-        />
-        <button onClick={this.props.addTodo}>Add Todo</button>
-        <button onClick={this.props.clearTodo}>Clear Completed</button>
+// method 1: class component
+// export default class TodoForm extends Component {
+//   render() {
+//     return (
+//       <div className="todoform">
+//         <input
+//           type="text"
+//           placeholder="...enter a task"
+//           value={this.props.value}
+//           onChange={this.props.changeHandler}
+//           onKeyPress={this.props.addOnEnter}
+//         />
+//         <button onClick={this.props.addTodo}>Add Todo</button>
+//         <button onClick={this.props.clearTodo}>Clear Completed</button>
+//       </div>
+//     );
+//   }
+// }
+
+// method 2: functional component: preferrable when we do not need state of any class methods
+const TodoForm = props => {
+  return (
+    <div className="todoform">
+      <input
+        type="text"
+        placeholder="...enter a task"
+        value={props.value}
+        onChange={props.changeHandler}
+        onKeyPress={props.addOnEnter}
+      />
+      <div className="button-wrapper">
+        <button onClick={props.addTodo}>Add</button>
+        <button onClick={props.clearTodo}>Clear</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,22 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import Todo from "./Todo";
+import './Todo.css';
 
-export default class TodoList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.todo.map(todo => {
-          return (
-            <Todo
-              key={todo.id}
-              id={todo.id}
-              task={todo.task}
-              completed={todo.completed}
-              markCompleted={this.props.markCompleted}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+// export default class TodoList extends Component {
+//   render() {
+//     return (
+//       <div className="todolist">
+//         {this.props.toDoCrate.map(todo => {
+//           return (
+//             <Todo
+//               key={todo.id}
+//               id={todo.id}
+//               task={todo.task}
+//               completed={todo.completed}
+//               markCompleted={this.props.markCompleted}
+//             />
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+const TodoList = props => {
+  return (
+    <div className="todolist">
+      {props.toDoCrate.filter(todo => todo.task.includes(props.searchString)
+      ).map(todo => {
+        return (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            task={todo.task}
+            completed={todo.completed}
+            markCompleted={props.markCompleted}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
